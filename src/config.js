896be10db74793e8bfc4ad06c604969a929f236c
Chris Wargo <Crwargo0783@gmail.com>
2020-04-28 14:52:12 -0400

    editted config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const API = process.env.REACT_APP_API_URL
+// export const API = process.env.REACT_APP_API_URL
+export const API =
+         window.location.hostname === 'localhost'
+           ? 'https://ecommercebonanzaapi.herokuapp.com/api'
+           : 'http://localhost:8000';
