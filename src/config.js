9f3de477e9f563286aed300bd96792ce2afd8b1d
Chris Wargo <Crwargo0783@gmail.com>
2020-04-28 16:37:45 -0400

    editted packages react start

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// export const API = process.env.REACT_APP_API_URL
+
 export const API =
          window.location.hostname === 'localhost'
            ? 'https://ecommercebonanzaapi.herokuapp.com/api'
