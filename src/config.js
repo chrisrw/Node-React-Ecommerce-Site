f104eaea2407dfd680bfe11c1251ea4e5e3fe825
Chris Wargo <Crwargo0783@gmail.com>
2020-04-28 16:53:10 -0400

    updated API path

--- src/config.js
+++ src/config.js
@@ -1,5 +1,1 @@
-
-export const API =
-         window.location.hostname === 'localhost'
-           ? 'https://ecommercebonanzaapi.herokuapp.com/api'
-           : 'http://localhost:8000';
+export const API = 'https://ecommercebonanzaapi.herokuapp.com/api';
